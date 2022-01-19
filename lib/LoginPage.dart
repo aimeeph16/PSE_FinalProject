3fc92d516039d242300933f6ed2e2ac3a1c90880
Callyrl <55436335+Callyrl@users.noreply.github.com>
2022-01-20 03:16:23 +0700

    Add files via upload

---
+++ lib/LoginPage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'Header.dart';
+import 'InputWrapper.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+            Color(0xFFF44786),
+            Color(0xF2F44786),
+            Color(0xE6F44786)
+          ]),
+        ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Header(),
+            Expanded(child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  )
+              ),
+              child: InputWrapper(),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
