112e431cb79a89659744c3f9f109c7a955886021
Rachel Wijaya <55416508+rachelwijaya@users.noreply.github.com>
2022-01-28 05:50:11 +0700

    Update auth_page.dart

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -70,6 +70,23 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return FlutterLogin(
       title: 'Secure',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.orange,
+        cursorColor: Colors.orange,
+        textTheme: TextTheme(
+          headline3: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 45.0,
+            color: Colors.orange,
+          ),
+          button: TextStyle(
+            fontFamily: 'OpenSans',
+          ),
+          subtitle1: TextStyle(fontFamily: 'NotoSans'),
+          bodyText2: TextStyle(fontFamily: 'NotoSans'),
+        ),
+      ),
       onLogin: _authUserLogin,
       onSignup: _authUserSignUp,
       onSubmitAnimationCompleted: () {
