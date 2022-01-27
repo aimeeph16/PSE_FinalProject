7ef3989eb0f5d8eea635ddc315291e84ab03b609
aimeeph16 <55416105+aimeeph16@users.noreply.github.com>
2022-01-27 16:44:33 +0700

    Add files via upload

--- camera/camera.py
+++ camera/camera.py
@@ -1,29 +1,26 @@
 import face_recognition
+import os
 import cv2
 import numpy as np
-import os
+
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 ds_factor=0.6
 
-#Store objects in array
-known_person=[] #Name of person string
-known_image=[] #Image object
-known_face_encodings=[] #Encoding object
+known_person=[]
+known_image=[] 
+known_face_encodings=[] 
 
-# Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
-process_this_frame = True
+frame_processed = True
 
-#Loop to add images in friends folder
 for file in os.listdir("profiles"):
     try:
-        #Extracting person name from the image filename eg: david.jpg
+        #extract person's name
         known_person.append(file.replace(".jpg", ""))
         file=os.path.join("profiles/", file)
         known_image = face_recognition.load_image_file(file)
-        #print("test")
         #print(face_recognition.face_encodings(known_image)[0])
         known_face_encodings.append(face_recognition.face_encodings(known_image)[0])
         #print(known_face_encodings)
@@ -45,26 +42,23 @@ class VideoCamera(object):
     def get_frame(self):
         success, image = self.video.read()
         
-        process_this_frame = True
-        
-            # Resize frame of video to 1/4 size for faster face recognition processing
+        frame_processed = True
         small_frame = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        #BGR to RGB color conversion (to allow face_recognition to be able to read it)
         rgb_small_frame = small_frame[:, :, ::-1]
         
-       # Only process every other frame of video to save time
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
+        #processing once every 2 frames
+        if frame_processed:
             face_locations = face_recognition.face_locations(rgb_small_frame)
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
             global name_gui;
             #face_names = []
             for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
+                #check for matching face
                 matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
+                name = "Unrecognized"
                 
                 #print(face_encoding)
                 print(matches)
@@ -82,21 +76,20 @@ class VideoCamera(object):
 
         process_this_frame = not process_this_frame
             
-# Display the results
+        #display
         for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
 
-            # Draw a box around the face
-            cv2.rectangle(image, (left, top), (right, bottom), (255, 255, 255), 2)
+        #drawing a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (255, 255, 255), 2)
 
-            # Draw a label with a name below the face
-            cv2.rectangle(image, (left, bottom - 35), (right, bottom), (255, 255, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(image, name_gui, (left + 10, bottom - 10), font, 1.0, (0, 0, 0), 1)
+        #labeling the box
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (255, 255, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name_gui, (left + 10, bottom - 10), font, 1.0, (0, 0, 0), 1)
 
         
         ret, jpeg = cv2.imencode('.jpg', image)
--- camera/main.py
+++ camera/main.py
@@ -44,6 +44,7 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
+
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
